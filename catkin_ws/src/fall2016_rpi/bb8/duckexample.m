4a2258148e0ef0f39ebeea88e3fde6fbba5b533a
Yiyang Li <liyiyanglinch@gmail.com>
2016-12-05 16:50:56 -0500

    close loop control

--- catkin_ws/src/fall2016_rpi/bb8/duckexample.m
+++ catkin_ws/src/fall2016_rpi/bb8/duckexample.m
@@ -1,15 +1,6 @@
 duck=RobotRaconteur.Connect('tcp://bb8.local:1234/DuckiebotServer.bb8/Duckiebot');
 
-duck.sendCmd(1.0,3.0); %the lager omega is, more left the duckbot will go. 
-                       %e.g 5.0 is too large
-pause(1.5);
-
+while (1)
+    duck.sendCmd(0.5,3.0);
+end
 duck.sendCmd(0.0,0.0);
-
-%{
-duck.sendCmd(0.8,-10);
-pause(0.5);
-duck.sendCmd(1.0,0.0);
-pause(1);
-duck.sendCmd(0.0,0.0);
-%}
