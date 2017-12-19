fc577333e26fc42040f922e946f3b2be97d794c6
Wailana <wailanabanana@gmail.com>
2017-12-19 07:00:46 +0000

    modified odometry with better math

--- catkin_ws/src/wifianalyzer_teamgrapes/src/heatmap_odometry_node.py
+++ catkin_ws/src/wifianalyzer_teamgrapes/src/heatmap_odometry_node.py
@@ -20,8 +20,8 @@ class VisualOdometry:
         self.writer = csv.writer(self.f)
         self.writer.writerow(('x','y','z'))
 
-        self.minNumFeatures = 500
-        self.lk_params = dict(winSize=(15,15), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+        self.minNumFeatures = 100
+        self.lk_params = dict(winSize=(21,21), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
         
         self.detector = cv2.FastFeatureDetector(threshold=25, nonmaxSuppression=True)
         
@@ -32,13 +32,13 @@ class VisualOdometry:
         self.D = ci.D
         self.K = ci.K
         self.K = np.matrix(self.K, dtype=np.float32)
-        self.K = K.reshape((3,3))
+        self.K = self.K.reshape((3,3))
         self.R = ci.R
         self.P = ci.P
         self.width = ci.width
         self.height = ci.height
         self.focal = self.K[0]
-        self.pp = (self.K[2], self.K[5])
+        self.pp = (self.K[0,2], self.K[1,2])
         self.camParamsSet = True
     
     # finds the essential matrix using K and the fundamental matrix
@@ -49,8 +49,10 @@ class VisualOdometry:
         # E = np.mat(F)
 
         # normalize image coordinates
-        normOldPts = cv2.undistortPoints(self.oldFeatures, self.K)
-        normNewPts = cv2.undistortPoints(self.newFeatures, self.K)
+        normOldPts = np.expand_dims(self.oldFeatures, axis=1)
+        normNewPts = np.expand_dims(self.newFeatures, axis=1)
+        normOldPts = cv2.undistortPoints(normOldPts, self.K, None)
+        normNewPts = cv2.undistortPoints(normNewPts, self.K, None)
 
         E, mask = cv2.findFundamentalMat(normOldPts, normNewPts, method=cv2.FM_RANSAC)
 
@@ -72,7 +74,8 @@ class VisualOdometry:
         Ra = np.mat(u) * W * np.mat(vt)
         Rb = np.mat(u) * W.T * np.mat(vt)
         t = u[:,2]
-
+        t = t.reshape((3,1))
+        
         # we now have four solutions
         Pa = np.concatenate((Ra, t), axis=1)
         Pb = np.concatenate((Ra, -1.0*t), axis=1)
@@ -82,24 +85,25 @@ class VisualOdometry:
         # solve for true solution by testing cheirality of each option
 
         # use only inliers to test cheirality
-        maskedOldFeatures = np.array(self.oldFeatures[mask == 1], dtype=np.float32)
-        maskedNewFeatures = np.array(self.newFeatures[mask == 1], dtype=np.float32)
+        maskedOldFeatures = self.oldFeatures[mask.ravel() == 1].astype(np.float32)
+        maskedNewFeatures = self.newFeatures[mask.ravel() == 1].astype(np.float32)
         # maskedOldFeatures = np.array(normOldPts[mask == 1], dtype=np.float32)
         # maskedNewFeatures = np.array(normNewPts[mask == 1], dtype=np.float32)
 
         # we assume the perspective matrix for our old frame is P = [I | 0]
         Po = np.mat([[1,0,0,0],[0,1,0,0],[0,0,1,0]], dtype=np.float32)
-                                            # 2xN array
+        
         p4d = cv2.triangulatePoints(Po, Pa, maskedOldFeatures[:1].T, maskedNewFeatures[:1].T)
-        Q = p4d[:,0]
+        Q = p4d[:,0].reshape((4,1))
 
         v = vt.T
         v_2 = -2*v
-        Ht = np.mat([[1,0,0,0],[0,1,0,0],[0,0,1,0],[v_2[0,2], v_2[1,2], v_2[2,2]]])
+        
+        Ht = np.mat([[1,0,0,0],[0,1,0,0],[0,0,1,0],[v_2[0,2], v_2[1,2], v_2[2,2], -1]], dtype=np.float32)
 
         # if c1 < 0, the point is behind the first camera
         c1 = Q[2] * Q[3]
-
+        
         # if c2 < 0, the point is behind the second camera
         c2 = (Pa * Q)[2] * Q[3]
 
@@ -125,8 +129,9 @@ class VisualOdometry:
         self.newFeatures = kp[st == 1]
 
     def publishPose(self):
-        print(self.cur_t)
-        x, y, z = (self.cur_t[0,0], self.cur_t[0,1], self.cur_t[0,2])
+        #print(self.cur_t)
+        #print(self.cur_t.shape)
+        x, y, z = (self.cur_t[0,0], self.cur_t[1,0], self.cur_t[2,0])
         rospy.loginfo(rospy.get_caller_id() + " robot is now at x:%f y:%f z:%f", x, y, z)
         self.writer.writerow((x,y,z))
 
@@ -171,13 +176,13 @@ class VisualOdometry:
         self.newImage = undistorted
 
         self.trackFeatures()
-        F, mask = cv2.findFundamentalMat(self.newFeatures, self.oldFeatures)
+        #F, mask = cv2.findFundamentalMat(self.newFeatures, self.oldFeatures)
         # _, R, t, mask = cv2.recoverPose(E, self.newFeatures, self.oldFeatures, focal=self.focal, pp=self.pp)
-        E = self.findEssentialMat(F)
-        R, t = self.recoverPose(E)
+        #E = self.findEssentialMat(F)
+        R, t = self.recoverPose()
 
-        self.cur_t = self.cur_t + self.cur_R.dot(t)
-        self.cur_R = self.cur_R.dot(R)
+        self.cur_t = self.cur_t + self.cur_R * t
+        self.cur_R = self.cur_R * R
         
         print('num features %d' % (self.newFeatures.shape[0]))
         if (self.newFeatures.shape[0] < self.minNumFeatures):
